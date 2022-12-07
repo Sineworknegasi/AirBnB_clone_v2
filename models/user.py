c4159512a1094a4bd35abe5f91b32f820a5fa731
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2022-12-07 13:48:28 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,8 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.review import Review
-from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -12,7 +10,8 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    places = relationship('Place', backref='user', cascade='delete')
+    reviews = relationship('Review', backref='user', cascade='delete')
