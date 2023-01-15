25537a1cd057fffe23b42b339d4fd6d4d633abdf
SINEWORK NEGASI <105476205+Sineworknegasi@users.noreply.github.com>
2023-01-16 00:50:06 +0300

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,74 +1,44 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
-"""import class Flask, render_template method"""
+"""
+written by: Sinework negasi
+cont:Write a script that starts a Flask web application
+"""
 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """displays text
-    Returns:
-        text
-    """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """displays text
-    Returns:
-        text
-    """
+@app.route("/hbnb")
+def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_C(text):
-    """displays text
-    Args:
-        text (str): text
-    Returns:
-        text
-    """
-    return 'C %s' % text.replace('_', ' ')
+@app.route("/c/<text>")
+def text(text):
+    return "C " + text.replace("_", " ")
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    """displays text
-    Args:
-        text (str): text
-    Returns:
-        text
-    """
-    return 'Python %s' % text.replace('_', ' ')
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is_cool"):
+    return "Python " + text.replace("_", " ")
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def display_num(n):
-    """displays text
-    Args:
-        n (int): number
-    Returns:
-        string
-    """
-    return "%d is a number" % n
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def display_HTML(n):
-    """displays text
-    Args:
-        n (int): number
-    Returns:
-        HTML page
-    """
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def temp_n(n):
+    return render_template("5-number.html", value=n)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
